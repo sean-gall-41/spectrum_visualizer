696bddf71b5bbbb21a8819643b79ccd87232f0ce
sean-gall-41 <galloglyst149@gmail.com>
2019-10-07 01:07:48 -0500

    changed launch.json, added playback to script

--- spectrum_analyzer.py
+++ spectrum_analyzer.py
@@ -2,12 +2,34 @@ import pyaudio
 import wave
 import sys #so we can utilize argv
 
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
+
 if len(sys.argv) < 2:
     print("Plays an audio file. \n\nUsage: python %s filename.[file_extension]"
         % sys.argv[0])
     sys.exit(-1)
 
-sound_file = wave.open(sys.argv[1], 'r')
+f = wave.open(sys.argv[1], 'r')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    format=p.get_format_from_width(f.getsampwidth()),
+    channels=f.getnchannels(),
+    rate=f.getframerate(),
+    output=True
+)
+
+data = f.readframes(CHUNK)
+
+while data != '':
+    stream.write(data)
+    data = f.readframes(CHUNK)
+
+stream.close()
+p.terminate()
 # import PySimpleGUI as gui
 
 # layout = [[gui.Button('Start')],[gui.Exit()]]
