f5c4509ecd8b71aac0a2afe99ea895ac6c354302
Sean Gallogly <galloglyst149@gmail.com>
2020-02-10 21:31:41 -0600

    added histogram, removed vis axes

--- audio_visualizer.py
+++ audio_visualizer.py
@@ -7,6 +7,7 @@ from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 from scipy.fftpack import fft
 from scipy.fftpack import fftfreq
+import argparse
 import math
 import struct
 
@@ -40,13 +41,11 @@ class Plot2D(object):
 
         pg.setConfigOptions(antialias=True)
         self.app = QtGui.QApplication([])
-        self.win = pg.GraphicsWindow(title="Frequency Analyzer")
-        self.win.resize(1000, 600)
-        self.win.setWindowTitle('Spectrum Analysis')
-        self.raw_waveform = self.win.addPlot(title='raw waveform', row=1,
-                                             col=1)
-        self.spectrum = self.win.addPlot(title='spectrum', row=2, col=1)
-
+        self.win = pg.GraphicsWindow(title="Music Visualizer")
+        self.win.resize(1280, 720)
+        self.spectrum = self.win.addPlot(row=1, col=1)
+        self.spectrum.hideAxis('bottom')
+        self.spectrum.hideAxis('left')
         self.CHUNK = 1024
 
         try:
@@ -72,7 +71,8 @@ class Plot2D(object):
             # scale t data to ms
             self.t_data = 1000 * self.t_data
             # find number of unique points to cut off graph at nyquist freq
-            self.n_unique_pts = int(math.ceil((self.CHUNK+1) / 2.))
+            # testing different total numbers of unique points
+            self.n_unique_pts = int(math.ceil((self.CHUNK+1) / 8.))
             # obtain f data in Hz
             self.f_data = np.arange(0, self.n_unique_pts, 1.) * \
                 (self.wf.getframerate() / self.CHUNK)
@@ -82,36 +82,14 @@ class Plot2D(object):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             QtGui.QApplication.instance().exec_()
 
-    def set_plotdata(self, name, dataset_x, dataset_y, hist=False):
-        # If plot 'name' is initialized, set data
+    def set_plotdata(self, name, dataset_x, dataset_y):
         if name in self.traces:
-            self.traces[name].setData(dataset_x, dataset_y)
-        # If plot 'name' is not initialized, add it to our traces dict and
-        # set up that plot item.
+            self.traces[name].setOpts(height=dataset_y)
         else:
-            if name == 'raw_waveform':
-                self.traces[name] = self.raw_waveform.plot(pen='b', width=3)
-                # y units in arbitrary units, normalized to lie in (-1., 1,)
-                self.raw_waveform.setYRange(-1, 1, padding=0)
-                # x units in ms
-                self.raw_waveform.setXRange(0, np.max(self.t_data),
-                                            padding=0.005)
-
-            if name == 'spectrum':
-                if hist:
-                    self.traces[name] = self.spectrum.plot(np.ones(2),
-                                                           np.ones(1),
-                                                           stepMode=True,
-                                                           brush=(0, 0, 255,
-                                                                  150))
-                else:
-                    self.traces[name] = self.spectrum.plot(pen='b', width=3)
-                self.spectrum.setXRange(0, self.wf.getframerate() / 2)
-                np.seterr(divide='ignore')
-                self.spectrum.setLogMode(x=True, y=False)
-                # TODO: not reaching maximum value? --> max value slightly off
-                # self.spectrum.setYRange(0., 1., padding=0)
-                # self.spectrum.setXRange(np.log10(20), 0, padding=0.005)
+            self.traces[name] = pg.BarGraphItem(x=dataset_x,
+                                                height=dataset_y,
+                                                width=1.5, brush='g')
+            self.spectrum.addItem(self.traces[name])
 
     def update(self):
         # read data one chunk at a time, write each chunk to output stream
@@ -126,28 +104,11 @@ class Plot2D(object):
 
         # Normalize wave data to lie within range (-1, 1)
         self.wav_data = self.wav_data / 2.**15
-        # take fft, find magn of result, and select (negative?) freq components
+        # take fft, find magn of result, and select pos freq components
         self.amp_data = np.abs(fft(self.wav_data)[0:self.n_unique_pts])
         # 'Normalization' and intensity correction for taking half total signal
         self.amp_data = (2. / self.CHUNK) * self.amp_data
-
-        # conversion to decibels I suppose and some data cleaning
-        # np.seterr(divide='ignore')
-        # self.amp_data = 20 * np.log10(self.amp_data)
-        # self.amp_data[np.isinf(self.amp_data)] = 0
-
-        # TODO: plot histogram data in spectrum plot item
-        # testing: code to calculate frequency of sine wave input
-        # freqs = fftfreq(len(self.amp_data))
-        # max_freq = freqs[np.argmax(np.abs(self.amp_data))]
-        # freq_Hz = abs(max_freq * self.wf.getframerate())
-        # with 440 signal, getting about 431...pretty close but why off?
-
-        self.set_plotdata(name='raw_waveform', dataset_x=self.t_data,
-                          dataset_y=self.wav_data,)
-
-        self.set_plotdata(name='spectrum', dataset_x=self.f_data,
-                          dataset_y=self.amp_data,)
+        self.set_plotdata("spectrum", self.f_data, self.amp_data)
 
     def animation(self):
         timer = QtCore.QTimer()
